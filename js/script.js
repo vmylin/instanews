1b8877e106c149dbf791a5f7350b1ec2a537eb72
Victor Lin <vlinmy06@gmail.com>
2019-10-29 21:45:58 -0700

    update js file, update html/css, insert working background-images into html.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    const articles = $('#articles');
+
     $('#news').on('change', function () {
-        // $('option').val().
+        let selection = $(this).val();
         $.ajax({
             method: 'GET',
-            //{home,arts,business,sports,technology,fashion}
-            url: 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=AWYIBsLV3ssb38vug8gBUciGOuyLBhEN'
-        })
-            .done(function (data) {
-                console.log(data.results);
-$.each(data.results, function() {
-    $('.article-list').append(`<a href="`+this.abstract+`"></a>`)
-    $('.article-list').append(`<li class="articles">`+this.abstract+`</li>`)
-    $('.articles').css(`background-image`,`url(`+ this.multimedia[4].url+ `)`)
-});
+            url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=AWYIBsLV3ssb38vug8gBUciGOuyLBhEN`,
+            dataType: "json"
+        }).done(function (data) {
+            console.log(data)
+            const removeObject = data.results.filter(function (e) {
+                if (e.multimedia[4] !== undefined) {
+                    return true;
+                }
             })
+            const cutArray = removeObject.slice(0, 12)
+            $.each(cutArray, function (index, value) {
+
+                articles.append(`
+                    <article class="each-article" style="background-image:url(${value.multimedia[4].url})">
+                        <a href="${value.url}"></a>
+                        <div class="title"><h2>${value.title}</h2>
+                        <p>${value.abstract}</p></div>
+                    </article>
+                    `);
+            });
+        })
             .fail(function () {
                 $('.user-name').append('Sorry there was an error.');
             });
-            //always method is used for something like a loading spinner
+        //always method is used for something like a loading spinner
     });
 });
 
 
 
 
-/*
-jQuery also has a document ready you can use
-NOTE all of these do the same thing, you just need one
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+// jQuery also has a document ready you can use
+// NOTE all of these do the same thing, you just need one
 
-OR the Shorthand
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
 
-$(function() {
-    console.log( "ready!" );
-});
-*/
+// OR the Shorthand
+
+// $(function() {
+//     console.log( "ready!" );
+// });
