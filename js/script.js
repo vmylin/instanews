2afe630a3447834a8ace28019a3936e561abf791
Victor Lin <vlinmy06@gmail.com>
2019-10-30 21:15:03 -0700

    update css: change from using position to using grid for the articles

--- js/script.js
+++ js/script.js
@@ -4,32 +4,45 @@ document.addEventListener('DOMContentLoaded', function () {
 
     $('#news').on('change', function () {
         let selection = $(this).val();
+        $('.load-gif').show();
         $.ajax({
             method: 'GET',
             url: `https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=AWYIBsLV3ssb38vug8gBUciGOuyLBhEN`,
             dataType: "json"
-        }).done(function (data) {
-            console.log(data)
-            const removeObject = data.results.filter(function (e) {
-                if (e.multimedia[4] !== undefined) {
-                    return true;
-                }
-            })
-            const cutArray = removeObject.slice(0, 12)
-            $.each(cutArray, function (index, value) {
-
-                articles.append(`
-                    <article class="each-article" style="background-image:url(${value.multimedia[4].url})">
-                        <a href="${value.url}"></a>
-                        <div class="title"><h2>${value.title}</h2>
-                        <p>${value.abstract}</p></div>
-                    </article>
-                    `);
-            });
         })
+            .done(function (data) {
+                $('.load-gif').hide();
+                console.log(data)
+                const removeObject = data.results.filter(function (e) {
+                    if (e.multimedia[4] !== undefined) {
+                        return true;
+                    }
+                })
+
+                articles.html("");
+
+                const cutArray = removeObject.slice(0, 12)
+
+                $.each(cutArray, function (index, value) {
+
+                    articles.append(`
+                    <a href="${value.url}">
+                        <article class="each-article" style="background-image:url(${value.multimedia[4].url})">
+                            
+                            <div class="title">
+                            <h2>${value.title}</h2>
+                            <p>${value.abstract}</p></div>
+                        </article>
+                    </a>
+                    `);
+                });
+            })
             .fail(function () {
-                $('.user-name').append('Sorry there was an error.');
-            });
+                $('.all-articles').append(`<p>Sorry there was an error.</p>`);
+            })
+            .always(function () {
+                //what i want to do in her is to for the svg loader....
+            })
         //always method is used for something like a loading spinner
     });
 });
